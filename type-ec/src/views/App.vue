2ce1062e92be7eceb754d5b61bb01b8aef655e61
naoki-kubota110 <naoki.kubota@rakus-partners.co.jp>
2021-06-15 15:38:37 +0900

    init

---
+++ type-ec/src/views/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-main>
+      <Header></Header>
+      <router-view/>
+      <div id="main">
+      <h1>hello</h1>
+
+      </div>
+    </v-main>
+</template>
+
+<script lang="ts">
+import { Component, Vue ,Watch, Prop} from 'vue-property-decorator';
+import Header from '../components/Header.vue'
+
+@Component({
+  components: {
+    Header
+  }
+})
+
+
+export default class App extends Vue{
+
+}
+</script>
